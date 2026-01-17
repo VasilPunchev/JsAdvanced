834239f4ebb2bc21b67d0e650b7bc6a59a72fd5c
Vasil Punchev <vpunchev85@gmail.com>
2026-01-17 18:00:24 +0200

    Objects and Composition - Lab

---
+++ 01.CityRecord.js
@@ -0,0 +1,15 @@
+function cityRecord(cityName,population,treasury){
+
+    let city = {
+        name:cityName,
+        population: population,
+        treasury: treasury
+    }
+    return city;
+
+}
+console.log(cityRecord('Tortuga',
+
+7000,
+
+15000));
